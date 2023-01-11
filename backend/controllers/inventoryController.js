d16da12d9f4df043d649e4e52dcdda842782c3c4
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-01-11 13:32:48 +0000

    Inventory controller created: Whole inventory can be seen, a single game in the inventory can be seen and a game can be created and added to database

---
+++ backend/controllers/inventoryController.js
@@ -0,0 +1,55 @@
+const Inventory = require("../models/inventoryModel");
+const mongoose = require("mongoose");
+
+// GET whole inventory
+const getInventory = async (req, res) => {
+  // Finds and returns all documents in the database in an array and sorts in alphabetical order
+  const inventory = await Inventory.find({}).sort({ title: 1 });
+
+  res.status(200).json(inventory);
+};
+
+// GET a single game
+const getGame = async (req, res) => {
+  // Grabbing ID property from the route parameter
+  const { id } = req.params;
+
+  // Checks if ID is valid
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Game could not be found" });
+  }
+
+  const game = await Inventory.findById(id);
+
+  // If game cannot be found then return error
+  if (!game) {
+    return res.status(404).json({ err: "Game could not be found" });
+  }
+
+  res.status(200).json(game);
+};
+
+// CREATE a game
+const createGame = async (req, res) => {
+  const { title, copies, releaseDate } = req.body;
+
+  // Add document to database
+  try {
+    // Creates a new document with 3 properties
+    const inventory = await Inventory.create({ title, copies, releaseDate });
+    // Sends the inventory document in json format and a status of 200 to say everything is ok
+    res.status(200).json(inventory);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// DELETE a game
+
+// UPDATE a game
+
+module.exports = {
+  getInventory,
+  getGame,
+  createGame,
+};
--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,33 +1,19 @@
 const express = require("express");
-const Inventory = require("../models/inventoryModel");
-
+const {
+  getInventory,
+  getGame,
+  createGame,
+} = require("../controllers/inventoryController");
 const router = express.Router();
 
 // GET all games
-router.get("/", (req, res) => {
-  res.json({ message: "GET all games" });
-});
+router.get("/", getInventory);
 
 // GET a single game
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single game" });
-});
+router.get("/:id", getGame);
 
 // POST a new game
-router.post("/", async (req, res) => {
-  const { title, copies, releaseDate } = req.body;
-
-  try {
-    // Creates a new document with 3 properties
-    const inventory = await Inventory.create({ title, copies, releaseDate });
-    // Sends the inventory document in json format and a status of 200 to say everything is ok
-    res.status(200).json(inventory);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-
-  res.json({ message: "POST a new game" });
-});
+router.post("/", createGame);
 
 // DELETE a game
 router.delete("/:id", (req, res) => {
